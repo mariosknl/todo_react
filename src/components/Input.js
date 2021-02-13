dd3286b5b577b1e01a4dcda14f6e168b70cf0038
mariosknl <mariosknl@outlook.com>
2021-02-13 09:27:04 +0200

    implementing edit functionality

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Input = ({ onChange, value, onBlur }) => (
   <input
+    required
     type='text'
     className='w-full p-3 ml-3 text-5xl font-bold text-center text-green-400 border-2 border-green-200 rounded-xl focus:outline-none focus:underline'
     onChange={onChange}
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,30 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from './Button';
 import Input from './Input';
 import Item from './Item';
 
-const Todo = ({ text, deleteHandler, id }) => {
+const Todo = ({ text, deleteHandler, id, setTodos }) => {
   const [newValue, setNewValue] = useState(text);
   const [editValue, setEditValue] = useState(false);
 
   const updateHandler = () => {
     setEditValue(true);
   };
+  useEffect(() => {}, [editValue]);
 
   return (
     <div className='flex flex-row w-full my-2'>
-      <Button type='button' onClick={deleteHandler}>
-        &times;
-      </Button>
       {editValue ? (
-        <Input
-          value={newValue}
-          className='border-gray-300'
-          onChange={(e) => setNewValue(e.target.value)}
-          onBlur={(e) => setNewValue(e.target.value)}
-        />
+        <>
+          <Button type='button' onClick={console.log(id)}>
+            +
+          </Button>
+          <Input
+            value={newValue}
+            className='border-gray-300'
+            onChange={(e) => setNewValue(e.target.value)}
+            onBlur={(e) => setNewValue(e.target.value)}
+          />
+        </>
       ) : (
-        <Item onClick={() => updateHandler(id)}>{text}</Item>
+        <>
+          <Button type='button' onClick={deleteHandler}>
+            &times;
+          </Button>
+          <Item onClick={() => updateHandler(id)}>{text}</Item>
+        </>
       )}
     </div>
   );
